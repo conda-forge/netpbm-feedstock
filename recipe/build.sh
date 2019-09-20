46d5496efa82b501070ad1d6bc0b41dd81bc87d1
Julio Campagnolo <juliocampagnolo@gmail.com>
2019-09-20 15:22:04 -0300

    added pkgconfig

--- recipe/build.sh
+++ recipe/build.sh
@@ -21,8 +21,30 @@ make
 make package pkgdir=${pkgdir} PKGMANDIR="share/man" install-run install-dev
 sed -i 's#/usr/bin/perl#/usr/bin/env perl#g' ${pkgdir}/bin/*
 
+# Copy files to prefix
 cp -rf $pkgdir/bin $PREFIX
 cp -rf $pkgdir/lib $PREFIX
 cp -rf $pkgdir/share $PREFIX
 cp -rf $pkgdir/include $PREFIX
 
+VERSION=`cat "${pkgdir}/VERSION"`
+DATADIR=$PREFIX/share/netpbm
+LINKDIR=$PREFIX/lib
+INCLUDEDIR=$PREFIX/include
+BINDIR=$PREFIX/bin
+
+# Config program
+conf_temp="buildtools/config_template"
+sed -i "s|@VERSION@|$VERSION|" $conf_temp
+sed -i "s|@DATADIR@|$DATADIR|" $conf_temp
+sed -i "s|@BINDIR@|$BINDIR|" $conf_temp
+sed -i "s|@LINKDIR@|$LINKDIR|" $conf_temp
+sed -i "s|@INCLUDEDIR@|$INCLUDEDIR|" $conf_temp
+cp -r $conf_temp $PREFIX/bin/netpbm-config
+
+# pkgconfig 
+pkgconf="buildtools/pkgconfig_template"
+sed -i "s|@VERSION@|$VERSION|" $pkgconf
+sed -i "s|@LINKDIR@|$LINKDIR|" $pkgconf
+sed -i "s|@INCLUDEDIR@|$INCLUDEDIR|" $pkgconf
+cp -f $pkgconf $PKG_CONFIG_PATH/netpbm.pc
